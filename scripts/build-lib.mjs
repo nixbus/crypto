dc87cb47e076eaa2879dd21fdc9ea0109f49b085
David Rey <david.rey.acosta@gmail.com>
2024-05-23 20:00:46 +0200

    chore: remove banner

--- scripts/build-lib.mjs
+++ scripts/build-lib.mjs
@@ -23,16 +23,6 @@ async function main() {
       ...options,
       outdir: './dist',
       format: 'esm',
-      banner: {
-        js: [
-          'import bannerPath from "path";',
-          'import { fileURLToPath } from "url";',
-          'import { createRequire as topLevelCreateRequire } from "module";',
-          'const require = topLevelCreateRequire(import.meta.url);',
-          'const __filename = fileURLToPath(import.meta.url);',
-          'const __dirname = bannerPath.dirname(__filename);',
-        ].join(''),
-      },
     }),
     build({
       ...options,
